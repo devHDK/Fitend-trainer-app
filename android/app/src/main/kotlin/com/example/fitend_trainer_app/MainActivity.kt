a8b1fb5385c830a9bee8f88002ce82f075ded6a6
devHDK <hyundo91082@gmail.com>
2024-02-29 23:01:01 +0900

    git cache clear

--- android/app/src/main/kotlin/com/example/fitend_trainer_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.fitend_trainer_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/thread/model/exception/exceptios.dart
+++
@@ -1,9 +0,0 @@
-class CommonException implements Exception {
-  final String message;
-
-  CommonException({required this.message});
-}
-
-class UploadException extends CommonException {
-  UploadException({required super.message});
-}
